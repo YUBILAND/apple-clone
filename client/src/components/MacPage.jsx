da4b2e5a08b409f9a4660d753507312ca9ffdca5
YUBILAND <yubiland922@gmail.com>
2024-11-18 15:46:42 -0500

    mac carousel fixed gap, overflow, and arrows

--- client/src/components/MacPage.jsx
+++ client/src/components/MacPage.jsx
@@ -697,14 +697,13 @@ const MacPage = () => {
                     <div className='pt-[150px] font-semibold text-6xl text-left store_dark_gray'>Get to know Mac.</div>
                     <div>
                     <Carousel
-                    // customTransition="transform 8000ms linear"
+                    customTransition="transform 500ms ease-in-out"
                     arrows={true}
                     draggable={false}
                     keyBoardControl
                     renderArrowsWhenDisabled={true}
                     renderDotsOutside={true}
                     responsive={responsive}
-                    // rtl={false}
                     className='py-24'
                     ssr={true}
                     > 
@@ -712,11 +711,13 @@ const MacPage = () => {
                         return (
                             <div onClick={() => showCarouselPopUp(itemNum)} className={`cursor-pointer relative w-fit transition-transform duration-300 hover:scale-105 shadow-lg rounded-3xl ${item.textColor === 'white' && 'text-white'}`}>
                                 <img className='rounded-3xl w-[405px] h-[740px] object-cover object-bottom' src={item.img} alt="" />
-                                <div className='absolute top-0 left-0 ml-8 mt-8'>
+                                <div className='absolute top-0 left-0 ml-8 mt-8 w-full'>
+                                {/* TopText */}
                                     <div className='text-lg font-semibold'>
                                     {item.topText}
                                     </div>
-                                    <h1 className={`font-semibold text-3xl ${item.bottomTextRainbow && 'text-transparent bg-clip-text bg-gradient-to-r from-[#088ef7] via-[#ca58d2] to-[#f55310]'}`}>{item.bottomText}</h1>
+                                {/* BottomText */}
+                                    <h1 className={`w-[60%] font-semibold text-[28px] leading-[2.1rem] ${item.bottomTextRainbow && '!w-full text-transparent bg-clip-text bg-gradient-to-r from-[#088ef7] via-[#ca58d2] to-[#f55310]'}`}>{item.bottomText}</h1>
                                 </div>
 
                                 <div className='absolute bottom-0 right-0 mr-4 mb-4'>
